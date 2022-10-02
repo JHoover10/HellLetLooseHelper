00321b6df0ded4868236db77a886199459766e68
Jeffrey Hoover <jeffrey.hoover@publix.com>
2022-10-02 10:36:35 -0400

    update rules

--- src/data/rules.ts
+++ src/data/rules.ts
@@ -7,12 +7,12 @@ export let offensive = {
 
     ],
     "offensiveTeam": [
-        "Cannot place supplies from supply truck in the two rows/columns of the map.",
+        "Cannot place supplies from supply truck in the two rows/columns of their side of the map.",
         "Recon team cannot reach artillery until the third/middle point is captured."
     ],
     "defensiveTeam": [
-        "Can place supplies from supply truck in the first row/column of the map.",
+        "Can place supplies from supply truck in the first row/column of their side of the map.",
         "Once the first point is captured the first two rows/columns will give you the \"TURN BACK NOW\" timer.",
-        "Except for the first point, players can enter the first two rows/columns of the enemy territory."
+        "After the first point, players can enter the first two rows/columns of the enemy territory."
     ]
 }
