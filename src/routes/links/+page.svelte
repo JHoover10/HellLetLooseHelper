0216d4e54bd9931637961f9430ef36f281b0a7e0
Jeffrey Hoover <jeffrey.hoover@publix.com>
2022-10-03 22:33:09 -0400

    add some more padding

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -2,7 +2,7 @@
     import { links } from '../../data/links'
 </script>
 
-<ul class="list-group">
+<ul class="list-group" style="padding: 16px;">
     {#each links as link}
         <li class="list-group-item"><a href={link.link} class="link-primary">{link.title}</a></li>
     {/each}
