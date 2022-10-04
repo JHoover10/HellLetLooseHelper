18e6cf1bd0dab929fa9135bfefa3a9db6142fa6b
Jeffrey Hoover <jeffrey.hoover@publix.com>
2022-10-03 23:46:53 -0400

    add more tips

--- src/data/tips.ts
+++ src/data/tips.ts
@@ -1,8 +1,7 @@
 export let tips = {
     "general": [
         "When going out of bounds players get 10 seconds to get back in, while vehicles get 30 seconds.",
-        "For classes/loadouts that get AP mines, make sure to place, you get more when you respawn.",
-        "Tanks cannot destroy engineer structures, but stachels can."
+        "For classes/loadouts that get AP mines, make sure to place them, you get more when you respawn."  
     ],
     "gameModes": {
         "offensive": [
@@ -15,7 +14,7 @@ export let tips = {
             "Rockets can destroy OPs and garrisons."
         ],
         "armor": [
-            "Always load armour piercing first.",
+            "When getting into a tank always load armour piercing first, you will never regret not having HE loaded.",
             "Players render at 500m and vehicles at 1000m.",
             "Once you have unlocked the loadout with the torch, you should always run it."
         ],
@@ -25,10 +24,19 @@ export let tips = {
         ],        
         "engineer": [
             "Repair stations also exist, try to make one every once in a while.",
-            "You can place supplies from a supply track past the second row/column and build nodes before the second row/column. This prevents them from being automatically destroyed if the enemy takes the second to last point on your side of the map."
+            "You can place supplies from a supply track past the second row/column and build nodes before the second row/column. This prevents them from being automatically destroyed if the enemy takes the second to last point on your side of the map.",
+            "Repair stations can be climbed on, which can allow your team to vault walls they would normally not be able to."
+        ],
+        "general": [
+            "Only the commander, officers, supports, armour teams, and recon teams can see the results of the recon plane. Unlike the spotters flare gun which everyone can see.",
+            "Tanks cannot destroy engineer structures, but stachels can."
         ],
         "officer": [
-            "Always be looking for or thinking about a place to put a new garrison down at."
+            "Always be looking for or thinking about a place to put a new garrison down at.",
+            "Your squad can only see markers you or the commander put down, please remark things as needed so your squad is also aware."
+        ],
+        "recon": [
+            "Recon squads do not show up on recon plane scans."
         ],
         "support": [
             "If you don't have a need for your supplies and their off cooldown, place them on point, your anti-tank/engineers/officers will love you for it.",
